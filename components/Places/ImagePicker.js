190687ec14ca35cf6b842c40eb8d20a41840b83d
Dinod Tharinda <94352564+dinodtharinda@users.noreply.github.com>
2024-09-16 12:10:08 +0530

    request permission and open camera

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,50 @@
+import { Alert, Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+
+function ImagePicker() {
+  const [cameraPermissionInfomation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInfomation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInfomation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficint Permissions!",
+        "You need to grant camera permission to use this app."
+      );
+      return false;
+    }
+
+    return true;
+  }
+  async function takeImageHandler() {
+    // specially for ios. need to verify permissions for ios
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image);
+  }
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
+}
+
+export default ImagePicker;
